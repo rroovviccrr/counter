5fb2a6bbea86ffe8ebd2565a01125c1045fdf86c
_rovi.cr <rovicr@live.com>
2021-03-29 03:33:52 +0000

    counter

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,23 +2,60 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import PropType from "prop-types";
 //create your first component
-export function Home() {
+export function Home(props) {
+	const styleCard = {
+		color: "white",
+		backgroundColor: "rgb(22,22,22)",
+		fontSize: "250px",
+		borderRadius: "20px",
+		textAlign: "center"
+	};
+	const styleBackgroud = {
+		backgroundColor: "rgb(7,7,7)"
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div style={styleBackgroud}>
+			<div className="d-flex justify-content-center">
+				<div className="row">
+					<div className="col" style={styleCard}>
+						<i className="far fa-clock" />
+					</div>
+
+					<div className="col" style={styleCard}>
+						{props.digit1}
+					</div>
+
+					<div className="col" style={styleCard}>
+						{props.digit2}
+					</div>
+
+					<div className="col" style={styleCard}>
+						{props.digit3}
+					</div>
+
+					<div className="col" style={styleCard}>
+						{props.digit4}
+					</div>
+
+					<div className="col" style={styleCard}>
+						{props.digit5}
+					</div>
+
+					<div className="col" style={styleCard}>
+						{props.digit6}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
+Home.propTypes = {
+	digit1: PropType.string,
+	digit2: PropType.string,
+	digit3: PropType.string,
+	digit4: PropType.string,
+	digit5: PropType.string,
+	digit6: PropType.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,27 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+var count = 0;
+var newCounter;
+function increaseCounter() {
+	count++;
+	newCounter = "000000" + count;
+	newCounter = newCounter.slice(-6);
+	console.log(newCounter);
+	if (count == 10) {
+		alert("You have reached your time limit!");
+		clearInterval(interval);
+	}
+	ReactDOM.render(
+		<Home
+			digit1={newCounter.charAt(0)}
+			digit2={newCounter.charAt(1)}
+			digit3={newCounter.charAt(2)}
+			digit4={newCounter.charAt(3)}
+			digit5={newCounter.charAt(4)}
+			digit6={newCounter.charAt(5)}
+		/>,
+		document.querySelector("#app")
+	);
+}
+var interval = setInterval(increaseCounter, 1000);
